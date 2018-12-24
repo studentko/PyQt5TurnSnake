8dd58f79c3590d2845a720d7d1db0bcb66ad40ee
Studentko <studentko.student@gmail.com>
2018-12-24 14:06:00 +0100

    EDrawable and direction in BaseBlock

--- GameMechanic/BaseBlock.py
+++ GameMechanic/BaseBlock.py
@@ -1,5 +1,13 @@
+from enum import Enum
+
+
 class BaseBlock:
     def __init__(self):
         self.x = -1
         self.y = -1
+        self.drawable = EDrawable.Default
+        self.direction = 0
 
+
+class EDrawable(Enum):
+    Default = 1
